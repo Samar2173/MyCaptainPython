169a2a770701bbc653c013ac60621d35e2b2a74e
Samar2173 <43983461+Samar2173@users.noreply.github.com>
2021-06-29 10:52:48 +0530

    Delete Task-2-Extension.py

--- Task-2-Extension.py
+++
@@ -1,9 +0,0 @@
-ext = {'py':'Python', 'c':'C', 'exe':'Executable File', 'gif':'Graphics Interchange Format'}
-
-fileName = str(input("Input the Filename: "))
-extFile = fileName.split('.')[1]
-
-if extFile in ext:
-    print(f'The extension of the file is: {ext[extFile]}')
-else:
-    print('Could not understand the format')
